f60a50ec6b7e2b1c6856424e2fbe8801833dd2bd
DEROUICHE Mohamed Ramy <mr.derouiche@soprahr.com>
2025-06-21 16:32:16 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App(){
+  return (
+    <div style={{padding: '2rem'}}>
+    <h1>SmartAssign</h1>
+    <p>Bienvenue sur votre plateforme d'affectation intelligente des tâches</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default function AboutUs(){
+    <div className="p-6 max-w-6xl mx-auto text-gray-800">
+        {/*section title*/}
+        
+    </div>
+}
