75924186eab50c44ca13c309dc61f3ff36a362dc
DEROUICHE Mohamed Ramy <mr.derouiche@soprahr.com>
2025-06-25 14:17:10 +0100

    Fix vite base for Github Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Navbar from './components/navbar';
-import Features from './components/Features';
 import About from './pages/About';
-import Contact from './components/Contact';
-import Footer from './components/Footer';
-
-
 function App() {
   return (
-    <div>
-      <h1>Hello!</h1>
-      <p>This is a test.</p>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+
+      </Routes>
+    </>
   );
 }
 
