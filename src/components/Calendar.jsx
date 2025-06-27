05adc7011ca0a7d530ec2c804a30b32eebbe052d
DEROUICHE Mohamed Ramy <mr.derouiche@soprahr.com>
2025-06-27 12:16:39 +0100

    Adjusted it

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import Scheduler from './node_modules/@bitnoi.se/react-scheduler/scheduler/index.js';
 
 function Calendar() {
   return (
     <div style={{ padding: '2rem' }}>
       <h2>Calendrier des tâches</h2>
-      <Scheduler />
     </div>
   );
 }
