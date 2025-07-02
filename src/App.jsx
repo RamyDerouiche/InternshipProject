88e2b75c4ee17a771c9046c0dd3f2fd4bb156fd6
DEROUICHE Mohamed Ramy <mr.derouiche@soprahr.com>
2025-07-02 11:11:04 +0100

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import React, {useState} from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Navbar from './components/Navbar';
 import Login from './components/Login';
 
-
-
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
   return (
-    <Router>
-      <Navbar loggedIn={loggedIn}/>
+    <>
+      <Navbar loggedIn={loggedIn} />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -19,7 +17,7 @@ function App() {
 
 
       </Routes>
-    </Router>
+    </>
   );
 }
 
