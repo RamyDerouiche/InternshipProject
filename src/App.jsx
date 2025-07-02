78b8398ec7612440239d485a8a3e3bd11ef6ddab
DEROUICHE Mohamed Ramy <mr.derouiche@soprahr.com>
2025-07-02 10:56:21 +0100

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
 import Login from './components/Login';
 
 
